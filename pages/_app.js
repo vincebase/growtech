dc84e1697edab6885cfe579cf5ebfc2a6ad2f9f2
Iamgelo <gelo.soliven01@gmail.com>
2021-07-15 21:08:59 +0800

    added files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css'
 
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> f65dbc2b22263ac8cc2bba08b029e11cc783dd79
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
