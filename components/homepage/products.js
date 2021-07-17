6110e7bc55b80f744b13658f9aee8eeea2d078cb
jamesvilleza <84057160+jamesvilleza@users.noreply.github.com>
2021-07-18 00:33:23 +0800

    added some text

--- components/homepage/products.js
+++ components/homepage/products.js
@@ -1,21 +1,14 @@
-
-import Productplants from "./productplant"
+import Productplants from "./productplant";
 
 const Products = () => {
     return (
         <div>
-            <div className="flex justify-center">
-                <span class="inline-block h-1 w-20 rounded bg-green-500 mt-6 mb-4"></span>
-                <h1 className="pt-4 font-bold mx-4 md:text-3xl md:pt-2" > OUR PRODUCTS</h1>
-                <span class="inline-block h-1 w-20 rounded bg-green-500 mt-6 mb-4"></span>
-            </div>
             <div className="flex justify-center  py-10">
                 <h2 className="md:text-3xl">15% goes to the farmers</h2>
             </div>
             <Productplants />
-
         </div>
-    )
-}
+    );
+};
 
-export default Products
+export default Products;
